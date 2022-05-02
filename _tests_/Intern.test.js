cb1753ba140107814c127c4f88ae862c9214d23d
Lucca Araujo <lucca.ara7@gmail.com>
2022-05-01 18:49:53 -0700

    add methods and tests to Intern

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,3 +1,24 @@
 // Properties: name, id, email, school
 //Methods: getName(), getId(), getEmail(), getRole(), getSchool()
 // return Employee
+
+const Intern = require("../lib/Intern");
+
+test("Testing if it is possible to set the school's name with the constructor", () => {
+  const testSchool = "UOFT";
+  const t = new Intern("John", 16, "testing@gmail.com", testSchool);
+  expect(t.school).toBe(testSchool);
+});
+
+//Methods
+test("Check if the getRole() method returns Intern", () => {
+  const testRole = "Intern";
+  const t = new Intern("John", 16, "testing@gmail.com", "UOFT");
+  expect(t.getRole()).toBe(testRole);
+});
+
+test("Check if the method getSchool() works", () => {
+  const testSchool = "UOFT";
+  const t = new Intern("John", 16, "testing@gmail.com", testSchool);
+  expect(t.getSchool()).toBe(testSchool);
+});
