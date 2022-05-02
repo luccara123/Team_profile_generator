21468cea5a81a84e5effd78e11e50651b12aefed
Lucca Araujo <lucca.ara7@gmail.com>
2022-05-01 18:34:48 -0700

    add tests and methods to Engineer

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,3 +1,23 @@
 // Properties: name, id, email, github (username)
 //Methods: getName(), getId(), getEmail(), getRole(), getGithub()
 // return Engineer
+
+const Engineer = require("../lib/Engineer");
+
+test("Testing if it is possible to set the github username with the constructor", () => {
+  const testGithub = "Test123";
+  const t = new Engineer("Peter", 8, "testing@gmail.com", testGithub);
+  expect(t.github).toBe(testGithub);
+});
+
+test("Check if the method getRole() returns Engineer", () => {
+  const testRole = "Engineer";
+  const t = new Engineer("Peter", 8, "testing@gmail.com", "Test123");
+  expect(t.getRole()).toBe(testRole);
+});
+
+test("Check if the method getGithub() works", () => {
+  const testGithub = "Test123";
+  const t = new Engineer("Peter", 8, "testing@test.com", testGithub);
+  expect(t.getGithub()).toBe(testGithub);
+});
